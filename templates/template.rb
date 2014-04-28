bc4715a614512ca9cf6e61f50c9c8ece5887d11c
Seulgi Kim <sk765@cornell.edu>
2014-04-27 18:14:29 -0700

    Added templates

---
+++ templates/template.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+# Get one number at a time
+N = STDIN.gets.to_i
+P = STDIN.gets.to_i
+
+# Get two numbers at once
+# You can also save it as an array
+r,c = gets.strip.split.map {|num| num.to_i}
+
+puts sum
