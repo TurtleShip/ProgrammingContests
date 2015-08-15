19cb6109997f0a87ede4d896247afb04abae789e
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-08-15 02:30:45 -0700

    Update SegmentTree.java

--- Library/DataStructures/SegmentTree.java
+++ Library/DataStructures/SegmentTree.java
@@ -1,81 +1,144 @@
-// source : Competitive Programming 3
+/*
+    Note to myself: Whenever I implement SegTree, I have a tendency to 
+    segTree[idx] = Math.min(segTree[idx], newVal) during updates. This is WRONG.
+    For an update, we are replacing an existing value with 'newVal'. So once we find an idx that
+    corresponds to the position that we need to update, do segTree[idx] = newVal
+*/
 
-class SegmentTree {         // the segment tree is stored like a heap array
-    private int[] st, A;
-    private int n;
+class SegMinTree {
+    int N;
+    int[] segTree;
 
-    public SegmentTree(int[] _A) {
-        A = _A;
-        n = A.length;                   // copy content for local usage
-        st = new int[4 * n];
-        for (int i = 0; i < 4 * n; i++) st[i] = 0; // create vector with length `len' and fill it with zeroes
-        build(1, 0, n - 1);                                  // recursive build
+
+    public SegMinTree(int[] arr) {
+        this.N = arr.length;
+        segTree = new int[4 * N + 1000];
+        build(1, 0, N - 1, arr);
     }
 
-    public int rmq(int i, int j) {
-        return rmq(1, 0, n - 1, i, j);
-    } // overloading
-
-    public int update_point(int idx, int new_value) {
-        return update_point(1, 0, n - 1, idx, new_value);
-    }
-
-    private int left(int p) {
-        return p << 1;
-    } // same as binary heap operations
-
-    private int right(int p) {
-        return (p << 1) + 1;
-    }
-
-    private void build(int p, int L, int R) {
-        if (L == R)                            // as L == R, either one is fine
-            st[p] = L;                                         // store the index
-        else {                                // recursively compute the values
-            build(left(p), L, (L + R) / 2);
-            build(right(p), (L + R) / 2 + 1, R);
-            int p1 = st[left(p)], p2 = st[right(p)];
-            st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+    private void build(int idx, int left, int right, int[] arr) {
+        if (left == right) {
+            segTree[idx] = arr[left];
+        } else {
+            int mid = (left + right) / 2;
+            build(left(idx), left, mid, arr);
+            build(right(idx), mid + 1, right, arr);
+            segTree[idx] = Math.min(segTree[left(idx)], segTree[right(idx)]);
         }
     }
 
-    private int rmq(int p, int L, int R, int i, int j) {          // O(log n)
-        if (i > R || j < L) return -1; // current segment outside query range
-        if (L >= i && R <= j) return st[p];               // inside query range
+    public int query(int left, int right) {
+        return query(1, 0, N - 1, left, right);
+    }
 
-        // compute the min position in the left and right part of the interval
-        int p1 = rmq(left(p), L, (L + R) / 2, i, j);
-        int p2 = rmq(right(p), (L + R) / 2 + 1, R, i, j);
-
-        if (p1 == -1) return p2;   // if we try to access segment outside query
-        if (p2 == -1) return p1;                               // same as above
-        return (A[p1] <= A[p2]) ? p1 : p2;
-    }          // as as in build routine
-
-    private int update_point(int p, int L, int R, int idx, int new_value) {
-        // this update code is still preliminary, i == j
-        // must be able to update range in the future!
-        int i = idx, j = idx;
-
-        // if the current interval does not intersect
-        // the update interval, return this st node value!
-        if (i > R || j < L)
-            return st[p];
-
-        // if the current interval is included in the update range,
-        // update that st[node]
-        if (L == i && R == j) {
-            A[i] = new_value; // update the underlying array
-            return st[p] = L; // this index
+    private int query(int idx, int curLeft, int curRight, int targetLeft, int targetRight) {
+        // Completely out of range
+        if (targetRight < curLeft || curRight < targetLeft) {
+            return Integer.MAX_VALUE;
         }
 
-        // compute the minimum position in the
-        // left and right part of the interval
-        int p1, p2;
-        p1 = update_point(left(p), L, (L + R) / 2, idx, new_value);
-        p2 = update_point(right(p), (L + R) / 2 + 1, R, idx, new_value);
+        if (targetLeft <= curLeft && curRight <= targetRight) {
+            return segTree[idx];
+        }
 
-        // return the position where the overall minimum is
-        return st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+        int mid = (curLeft + curRight) / 2;
+        return Math.min(query(left(idx), curLeft, mid, targetLeft, targetRight),
+            query(right(idx), mid + 1, curRight, targetLeft, targetRight));
+    }
+
+    public void update(int pos, int val) {
+        update(1, 0, N - 1, pos, val);
+    }
+
+    private void update(int idx, int left, int right, int pos, int val) {
+
+        if (left == right) {
+            if (pos == right) segTree[idx] = val;
+            return;
+        }
+
+        if (left <= pos && pos <= right) {
+            int mid = (left + right) / 2;
+            update(left(idx), left, mid, pos, val);
+            update(right(idx), mid + 1, right, pos, val);
+            segTree[idx] = Math.min(segTree[left(idx)], segTree[right(idx)]);
+        }
+    }
+
+    private int left(int idx) {
+        return idx << 1;
+    }
+
+    private int right(int idx) {
+        return (idx << 1) + 1;
+    }
+}
+
+
+class SegMaxTree {
+    int N;
+    int[] segTree;
+
+
+    public SegMaxTree(int[] arr) {
+        this.N = arr.length;
+        segTree = new int[4 * N + 1000];
+        build(1, 0, N - 1, arr);
+    }
+
+    private void build(int idx, int left, int right, int[] arr) {
+        if (left == right) {
+            segTree[idx] = arr[left];
+        } else {
+            int mid = (left + right) / 2;
+            build(left(idx), left, mid, arr);
+            build(right(idx), mid + 1, right, arr);
+            segTree[idx] = Math.max(segTree[left(idx)], segTree[right(idx)]);
+        }
+    }
+
+    public int query(int left, int right) {
+        return query(1, 0, N - 1, left, right);
+    }
+
+    private int query(int idx, int curLeft, int curRight, int targetLeft, int targetRight) {
+        // Completely out of range
+        if (targetRight < curLeft || curRight < targetLeft) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (targetLeft <= curLeft && curRight <= targetRight) {
+            return segTree[idx];
+        }
+
+        int mid = (curLeft + curRight) / 2;
+        return Math.max(query(left(idx), curLeft, mid, targetLeft, targetRight),
+            query(right(idx), mid + 1, curRight, targetLeft, targetRight));
+    }
+
+    public void update(int pos, int val) {
+        update(1, 0, N - 1, pos, val);
+    }
+
+    private void update(int idx, int left, int right, int pos, int val) {
+
+        if (left == right) {
+            if (left == pos) segTree[idx] = val;
+            return;
+        }
+        if (left <= pos && pos <= right) {
+            int mid = (left + right) / 2;
+            update(left(idx), left, mid, pos, val);
+            update(right(idx), mid + 1, right, pos, val);
+            segTree[idx] = Math.max(segTree[left(idx)], segTree[right(idx)]);
+        }
+    }
+
+    private int left(int idx) {
+        return idx << 1;
+    }
+
+    private int right(int idx) {
+        return (idx << 1) + 1;
     }
 }
