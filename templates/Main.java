e0101ab1092eafc5e97c98bc6d63cffb10912aa3
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-08-15 21:38:46 -0700

    Add a Java Template.

---
+++ templates/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+        final PrintWriter writer = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int T = scanner.nextInt();
+
+        for (int cid = 1; cid <= T; cid++) {
+            writer.printf("Case %d:\n", cid);
+        }
+        writer.flush();
+    }
+}
