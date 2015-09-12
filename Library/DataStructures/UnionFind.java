6f81ec6bf3642d7b6b8c86b1e24f4fe9f07fb5f7
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-09-11 21:44:52 -0700

    Update UnionFind.java to use int array rather than Vector

--- Library/DataStructures/UnionFind.java
+++ Library/DataStructures/UnionFind.java
@@ -1,27 +1,24 @@
 // Shameless copy from Competitive Programming 3
 class UnionFind {
-    private Vector<Integer> p, rank, setSize;
+    //    private List<Integer> p, rank, setSize;
+    private int[] p, rank, setSize;
     private int numSets;
 
     public UnionFind(int N) {
-        p = new Vector<Integer>(N);
-        rank = new Vector<Integer>(N);
-        setSize = new Vector<Integer>(N);
+        p = new int[N];
+        rank = new int[N];
+        setSize = new int[N];
         numSets = N;
         for (int i = 0; i < N; i++) {
-            p.add(i);
-            rank.add(0);
-            setSize.add(1);
+            p[i] = i;
+            rank[i] = 0;
+            setSize[i] = 1;
         }
     }
 
     public int findSet(int i) {
-        if (p.get(i) == i) return i;
-        else {
-            int ret = findSet(p.get(i));
-            p.set(i, ret);
-            return ret;
-        }
+        if (p[i] == i) return i;
+        return p[i] = findSet(p[i]);
     }
 
     public Boolean isSameSet(int i, int j) {
@@ -33,13 +30,13 @@ class UnionFind {
             numSets--;
             int x = findSet(i), y = findSet(j);
             // rank is used to keep the tree short
-            if (rank.get(x) > rank.get(y)) {
-                p.set(y, x);
-                setSize.set(x, setSize.get(x) + setSize.get(y));
+            if (rank[x] > rank[y]) {
+                p[y] = x;
+                setSize[x] += setSize[y];
             } else {
-                p.set(x, y);
-                setSize.set(y, setSize.get(y) + setSize.get(x));
-                if (rank.get(x) == rank.get(y)) rank.set(y, rank.get(y) + 1);
+                p[x] = y;
+                setSize[y] += setSize[x];
+                if (rank[x] == rank[y]) rank[y]++;
             }
         }
     }
@@ -49,6 +46,6 @@ class UnionFind {
     }
 
     public int sizeOfSet(int i) {
-        return setSize.get(findSet(i));
+        return setSize[findSet(i)];
     }
 }
