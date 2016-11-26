dd7df0726372f88683e3fc4dd9858ae392c1273f
TurtleShip <seulgi.kim.56@outlook.com>
2016-11-25 17:06:26 -0800

    Added a template for Java for fast io.

---
+++ templates/FastIO.java
@@ -0,0 +1,114 @@
+import java.io.*;
+import java.util.InputMismatchException;
+
+// Use this in case IO restriction is toooooo tight
+public class FastIO {
+    static InputReader br;
+    static PrintWriter pw;
+
+    public static void main(String[] args) throws IOException {
+        br = new InputReader(System.in);
+        pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        // put your code to solve the problem here
+        exitImmediately();
+    }
+
+    private static void exitImmediately() {
+        pw.close();
+        System.exit(0);
+    }
+
+    static class InputReader {
+
+        private InputStream stream;
+        private byte[] buf = new byte[102400];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                exitImmediately();
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double readDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+    }
+}
